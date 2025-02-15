f6aca6e8d709fffb048f1f3bbd33badefc53f224
Transport for Bandung <transportforbandung@gmail.com>
2025-02-16 02:20:41 +0700

    Update fetch-script.js

--- components/fetch-script.js
+++ components/fetch-script.js
@@ -1,4 +1,5 @@
 function loadComponent(url, elementId, callback) {
+  console.log(`Loading ${url} into #${elementId}...`);
   fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -7,6 +8,7 @@ function loadComponent(url, elementId, callback) {
       return response.text();
     })
     .then(data => {
+      console.log(`Successfully loaded ${url}`);
       document.getElementById(elementId).innerHTML = data;
       if (callback) callback();
     })
@@ -15,11 +17,15 @@ function loadComponent(url, elementId, callback) {
     });
 }
 
-// Load header and footer
-loadComponent('./components/header.html', 'header', () => {
-  // Initialize hamburger after header loads
+// Load header and footer using root-relative paths
+loadComponent('/components/header.html', 'header', () => {
+  console.log('Header loaded. Initializing hamburger...');
   if (typeof initializeHamburger === 'function') {
     initializeHamburger();
+  } else {
+    console.error('initializeHamburger is not defined.');
   }
 });
-loadComponent('./components/footer.html', 'footer');
+loadComponent('/components/footer.html', 'footer', () => {
+  console.log('Footer loaded.');
+});
