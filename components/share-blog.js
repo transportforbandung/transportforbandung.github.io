074c38cd9488eb1864656d9aa4cf06a302d46f9e
Transport for Bandung <transportforbandung@gmail.com>
2025-02-16 03:16:53 +0700

    Create share-blog.js

---
+++ components/share-blog.js
@@ -0,0 +1,11 @@
+<script>
+  // Get the current page's URL and title
+  const currentUrl = encodeURIComponent(window.location.href);
+  const currentTitle = encodeURIComponent(document.title);
+
+  // Update the href attributes of the share buttons
+  document.getElementById('share-facebook').href = `https://facebook.com/sharer/sharer.php?u=${currentUrl}`;
+  document.getElementById('share-twitter').href = `https://twitter.com/intent/tweet?url=${currentUrl}&text=${currentTitle}`;
+  document.getElementById('share-linkedin').href = `https://www.linkedin.com/shareArticle?url=${currentUrl}&title=${currentTitle}`;
+  document.getElementById('share-whatsapp').href = `https://wa.me/?text=${currentTitle}%20${currentUrl}`;
+</script>
