166538c6dbd3bf4c2fc036cd6789a041786c68af
Transport for Bandung <transportforbandung@gmail.com>
2025-02-25 18:16:10 +0700

    Create gtag.js

---
+++ components/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MNSZN2RKR0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MNSZN2RKR0');
+</script>
