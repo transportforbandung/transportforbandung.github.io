f8466efd105eccc5cbb497db448d6ba8c69a0ed1
Transport for Bandung <transportforbandung@gmail.com>
2025-03-08 06:03:32 +0700

    Update and rename fetch-routes.js to fetch-new-routes.js

--- fetch-new-routes.js
+++ fetch-new-routes.js
@@ -91,8 +91,8 @@ async function processRoute({ relationId, displayType }) {
 
   // Determine and fetch stops
   const stopsQuery = displayType === 'ways_with_points' 
-    ? `[out:json]; relation(${relationId}); node(r:"stop"); out geom;`
-    : `[out:json]; relation(${relationId}); node(r:"stop_entry_only"); node(r:"stop_exit_only"); out geom;`;
+    ? `[out:json];relation(${relationId});node(r:"stop");out geom;relation(${relationId});node(r:"stop_entry_only");out geom;relation(${relationId});node(r:"stop_exit_only");out geom;`
+    : `[out:json];relation(${relationId});node(r:"stop_entry_only");out geom;relation(${relationId});node(r:"stop_exit_only");out geom;`;
 
   const stopsData = await overpassQuery(stopsQuery);
   const fileName = displayType === 'ways_with_points' ? 'stops.geojson' : 'endstops.geojson';
