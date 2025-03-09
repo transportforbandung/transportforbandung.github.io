51edbf2f4790d962dddd4eaa72391f885999fa9f
Transport for Bandung <transportforbandung@gmail.com>
2025-03-10 03:43:29 +0700

    Delete components/gtag.js

--- components/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MNSZN2RKR0"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MNSZN2RKR0');
-</script>
