d314d87e3ed9db565212f0f2349ef811f14b16fb
Transport for Bandung <transportforbandung@gmail.com>
2025-04-20 19:53:52 +0700

    Create svgo.config.js

---
+++ action-scripts/svgo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // Customize plugins here
+          removeViewBox: false,
+        },
+      },
+    },
+  ],
+};
