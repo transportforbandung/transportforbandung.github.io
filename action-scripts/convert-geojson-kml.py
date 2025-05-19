fd514576c29ebe423d4e9dee6661ab80574c7de5
NFarrasP <naufalfarrashariyanto@gmail.com>
2025-05-19 15:57:02 +0700

    Moved route file directory

--- action-scripts/convert-geojson-kml.py
+++ action-scripts/convert-geojson-kml.py
@@ -16,7 +16,8 @@ def convert_hex_to_kml_color(hex_color):
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(base_dir, '..', 'data')
+    data_dir = os.path.join(base_dir, '..', 'route-data')
+    route_data_dir = os.path.join(data_dir, 'geojson')
     routes_json_path = os.path.join(data_dir, 'routes.json')
     output_dir = os.path.join(data_dir, 'kml-named')
     os.makedirs(output_dir, exist_ok=True)
@@ -30,7 +31,7 @@ def main():
             relation_id = route['relationId']
             route_name = route['name']
             color = route['color']
-            route_dir = os.path.join(data_dir, relation_id)
+            route_dir = os.path.join(route_data_dir, relation_id)
 
             if not os.path.exists(route_dir):
                 print(f"Skipping missing directory: {route_dir}")
